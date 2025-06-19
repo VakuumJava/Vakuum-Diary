2d3e4e4d256ada5753130626c3aa7b3f886445eb
VakuumJava <157950397+VakuumJava@users.noreply.github.com>
2025-06-19 20:29:42 +0300

    Levak

--- app.py
+++ app.py
@@ -183,4 +183,5 @@ if __name__ == '__main__':
             admin.email = admin_em
             admin.set_password(admin_pw)
         db.session.commit()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
